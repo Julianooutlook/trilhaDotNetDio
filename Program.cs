cb2c74566ca4740d107bbcf3ecda49b1c7d8c88e
juliano <julianopati92@outlook.com>
2024-11-02 15:21:23 -0300

    ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Nokia nokia = new Nokia("123456", "Nokia Modelo X", "11111111111111", 64);
+        Iphone iphone = new Iphone("654321", "iPhone Modelo Y", "2222222222222", 128);
+
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Telegram");
+    }
+}
